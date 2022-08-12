6afe052d9cfc68b17fb39ccf912eb4100aeb7a2f
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-08-12 19:10:32 +0200

    scss refactor

--- src/common/services/emitter.js
+++ src/common/services/emitter.js
@@ -1,52 +1,50 @@
 class _Emitter {
-  constructor() {
-    this.events = {}
-  }
-
-  // Register an event listener
-  on(event, listener) {
-    if (typeof this.events[event] !== 'object') {
-      this.events[event] = []
+    constructor() {
+        this.events = {}
     }
-    this.events[event].push(listener)
-    return () => this.off(event, listener)
-  }
 
-  // Unregister an event listener
-  off(event, listener) {
-    if (typeof this.events[event] === 'object') {
-      const idx = this.events[event].indexOf(listener)
-      if (idx > -1) {
-        this.events[event].splice(idx, 1)
-      }
+    // Register an event listener
+    on(event, listener) {
+        if (typeof this.events[event] !== 'object') {
+            this.events[event] = []
+        }
+        this.events[event].push(listener)
+        return () => this.off(event, listener)
     }
-  }
 
-  // Register an event listener which will be destroyed once it is called
-  once(event, listener) {
-    const remove = this.on(event, (...args) => {
-      remove()
-      listener.apply(this, args)
-    })
-  }
+    // Unregister an event listener
+    off(event, listener) {
+        if (typeof this.events[event] === 'object') {
+            const idx = this.events[event].indexOf(listener)
+            if (idx > -1) {
+                this.events[event].splice(idx, 1)
+            }
+        }
+    }
 
-  // Emit an event
-  emit(event, ...args) {
-    const nameSpaces = event.indexOf('.') !== -1 ? event.split('.') : [event]
-    let partialNameSpace = nameSpaces[0]
+    // Register an event listener which will be destroyed once it is called
+    once(event, listener) {
+        const remove = this.on(event, (...args) => {
+            remove()
+            listener.apply(this, args)
+        })
+    }
 
-    nameSpaces.forEach(name => {
-      if (partialNameSpace !== name) {
-        partialNameSpace += `.${name}`
-      }
+    // Emit an event
+    emit(event, ...args) {
+        const nameSpaces = event.indexOf('.') !== -1 ? event.split('.') : [event]
+        let partialNameSpace = nameSpaces[0]
 
-      if (typeof this.events[partialNameSpace] === 'object') {
-        this.events[partialNameSpace].forEach(listener =>
-          listener.apply(this, args)
-        )
-      }
-    })
-  }
+        nameSpaces.forEach((name) => {
+            if (partialNameSpace !== name) {
+                partialNameSpace += `.${name}`
+            }
+
+            if (typeof this.events[partialNameSpace] === 'object') {
+                this.events[partialNameSpace].forEach((listener) => listener.apply(this, args))
+            }
+        })
+    }
 }
 
 export const Emitter = new _Emitter()
