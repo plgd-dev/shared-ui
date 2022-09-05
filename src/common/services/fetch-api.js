4faae625fac7f70a1d7897b1610f02aa5ea3da47
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-09-05 23:05:53 +0200

    fetch-api.js fix

--- src/common/services/fetch-api.js
+++ src/common/services/fetch-api.js
@@ -19,10 +19,12 @@ export const fetchApi = async (url, options = {}) => {
     const { useSecurity } = security.getGeneralConfig()
     const { audience: defaultAudience } = security.getWebOAuthConfig()
     // Access token must be gathered and set as a Bearer header in all requests
-    const accessToken = useSecurity ? await security.getAccessTokenSilently()({
-        audience: audience || defaultAudience,
-        scope: scopes?.join?.(','),
-    }) : null
+    const accessToken = useSecurity
+        ? await security.getAccessTokenSilently()({
+            audience: audience || defaultAudience,
+            scope: scopes?.join?.(','),
+        })
+        : null
     const oAuthSettings = {
         ...fetchOptions,
         headers: {
