47114e6d8bc4522ae1fa7a722e14bf4c7f1bfa26
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-10-05 08:11:14 +0200

    add storybook

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Button from '../components/new/Button'
 import { ReactComponent as IconPlus } from './assets/icon-plus.svg'
+import './button.css'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -20,18 +21,18 @@ export default {
 const Template = (args) => (
     <div>
         <Button {...args} variant='primary' size='big' children='Primary big' />
-        <Button {...args} variant='primary' size='normal' children='Primary big' />
-        <Button {...args} variant='primary' size='small' children='Primary big' />
+        <Button {...args} variant='primary' size='normal' children='Primary normal' />
+        <Button {...args} variant='primary' size='small' children='Primary small' />
         <br />
         <br />
         <Button {...args} variant='secondary' size='big' children='Secondary big' />
-        <Button {...args} variant='secondary' size='normal' children='Secondary big' />
-        <Button {...args} variant='secondary' size='small' children='Secondary big' />
+        <Button {...args} variant='secondary' size='normal' children='Secondary normal' />
+        <Button {...args} variant='secondary' size='small' children='Secondary small' />
         <br />
         <br />
         <Button {...args} variant='tertiary' size='big' children='Tertiary big' />
-        <Button {...args} variant='tertiary' size='normal' children='Tertiary big' />
-        <Button {...args} variant='tertiary' size='small' children='Tertiary big' />
+        <Button {...args} variant='tertiary' size='normal' children='Tertiary normal' />
+        <Button {...args} variant='tertiary' size='small' children='Tertiary small' />
     </div>
 )
 
