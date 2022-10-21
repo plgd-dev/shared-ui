3a78541cfa14857abbf0c748b855eea536038e6f
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-10-21 22:42:05 +0200

    test

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,11 +14,7 @@ module.exports = {
   ],
   "framework": "@storybook/react",
   core: {
-    builder: 'webpack5',
-    options: {
-      lazyCompilation: true,
-      fsCache: true,
-    }
+    builder: 'webpack5'
   },
   babel: async (options) => {
     options.plugins.push('babel-plugin-inline-react-svg');
@@ -26,18 +22,18 @@ module.exports = {
   },
   webpackFinal: async (config) => {
     config.resolve.alias = {
-      ...config.resolve.alias,
-      "swiper/css/autoplay$": path.resolve(__dirname, "../../../node_modules/swiper/modules/autoplay/autoplay.min.css"),
-      "swiper/css/pagination$": path.resolve(__dirname, "../../../node_modules/swiper/modules/pagination/pagination.min.css"),
-      "swiper/css$": path.resolve(__dirname, "../../../node_modules/swiper/swiper.min.css"),
+      ...(config.resolve.alias || []),
+        // "swiper/css/autoplay": path.resolve(__dirname, "../../../node_modules/swiper/modules/autoplay/autoplay.min.css"),
+        // "swiper/css/pagination": path.resolve(__dirname, "../../../node_modules/swiper/modules/pagination/pagination.min.css"),
+        // "swiper/css": path.resolve(__dirname, "../../../node_modules/swiper/swiper.min.css"),
     };
-    config.resolve.plugins = [
-      ...(config.resolve.plugins || []),
-      new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, "../tsconfig.json"),
-        extensions: config.resolve.extensions,
-      }),
-    ];
+    // config.resolve.plugins = [
+    //   ...(config.resolve.plugins || []),
+    //   new TsconfigPathsPlugin({
+    //     configFile: path.resolve(__dirname, "../tsconfig.json"),
+    //     extensions: config.resolve.extensions,
+    //   }),
+    // ];
     config.resolve.extensions.push(".ts", ".tsx");
     // As the SVG loader may conflict with the file loader
     // We add .svg as a excluded extension for the file loader
