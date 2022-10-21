65bc251d8a810329bab956ab9b03af8f90f71cda
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-10-21 17:05:44 +0200

    test

--- .storybook/main.js
+++ .storybook/main.js
@@ -25,7 +25,7 @@ module.exports = {
       ...config.resolve.alias,
       "swiper/css/autoplay": path.resolve(__dirname, "../../../node_modules/swiper/modules/autoplay/autoplay.min.css"),
       "swiper/css/pagination": path.resolve(__dirname, "../../../node_modules/swiper/modules/pagination/pagination.min.css"),
-      "swiper/css": path.resolve(__dirname, "../../../node_modules/swiper/swiper-bundle.css"),
+      "swiper/css": path.resolve(__dirname, "../../../node_modules/swiper/swiper.min.css"),
     };
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
