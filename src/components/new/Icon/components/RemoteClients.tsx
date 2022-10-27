757eb8c5c20cbba679491b5363d6795f06d602ac
Patrik Matiaško <patrik.matiasko@gmail.com>
2022-10-27 22:31:44 +0200

    LeftPanel.stories.jsx update

--- src/components/new/Icon/components/RemoteClients.tsx
+++ src/components/new/Icon/components/RemoteClients.tsx
@@ -8,12 +8,12 @@ const SvgRemoteClients = (props: SVGProps<SVGSVGElement>) => (
         <g clipPath='url(#remote-clients_svg__a)'>
             <path
                 d='M0 15.214C0 18.408 2.419 21 5.4 21h13.8c2.651 0 4.8-2.302 4.8-5.143 0-2.487-1.65-4.564-3.84-5.038.154-.43.24-.9.24-1.39 0-2.13-1.612-3.858-3.6-3.858-.739 0-1.429.242-1.999.651C13.762 4.294 11.824 3 9.6 3c-3.315 0-6 2.877-6 6.429 0 .108.004.217.007.325-2.1.792-3.607 2.937-3.607 5.46Z'
-                fill='#191A1A'
+                fill='currentcolor'
             />
         </g>
         <defs>
             <clipPath id='remote-clients_svg__a'>
-                <path fill='#fff' d='M0 0h24v24H0z' />
+                <path fill='currentcolor' d='M0 0h24v24H0z' />
             </clipPath>
         </defs>
     </svg>
