0e8b1706458634ce7cb3b826e968979e4b77d659
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-03-14 14:55:23 +0100

    user widget style

--- src/components/new/Layout/Header/UserWidget/UserWidget.tsx
+++ src/components/new/Layout/Header/UserWidget/UserWidget.tsx
@@ -1,12 +1,18 @@
 import { FC } from 'react'
 import { Props } from './UserWidget.types'
 import * as styles from './UserWidget.styles'
+import Avatar from 'react-avatar'
 
 const UserWidget: FC<Props> = (props) => {
     const { name, description, image } = props
     return (
         <div css={styles.userWidget}>
             {image && <div css={styles.image}>{image}</div>}
+            {!image && (
+                <div css={styles.image}>
+                    <Avatar name={name} size='44' round='50%' color='#255897' />
+                </div>
+            )}
             <div>
                 <div css={styles.name}>{name}</div>
                 {description && <div css={styles.description}>{description}</div>}
