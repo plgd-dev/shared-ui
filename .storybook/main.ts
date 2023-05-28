25be033634ff5f432457f8c861a0900a603b5df5
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-05-28 23:14:31 +0200

    test

---
+++ .storybook/main.ts
@@ -0,0 +1,39 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/preset-scss',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-themes'
+  ],
+  framework: '@storybook/react-webpack5',
+  babel: async (options:any) => {
+    options.plugins.push('babel-plugin-inline-react-svg')
+    return options;
+  },
+  // webpackFinal: async config => {
+  //   config.resolve.alias = {
+  //     ...(config.resolve.alias || [])
+  //   };
+  //   config.resolve.extensions.push('.ts', '.tsx')
+  //   return config
+  // },
+  core: {
+    builder: {
+      name: '@storybook/builder-webpack5',
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
+  },
+  staticDirs: ['../public'],
+  docs: {
+    autodocs: true
+  }
+}
+
+export default config
