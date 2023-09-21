9f71a60d77ae868e9c1aa808a63af99f2d419619
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-09-21 23:38:20 +0200

    Theme update

--- src/components/Atomic/_theme/index.ts
+++ src/components/Atomic/_theme/index.ts
@@ -1,5 +1,6 @@
 import light from './light'
 import lGet from 'lodash/get'
+import siemens from './siemens'
 
 export type ThemeType = Partial<typeof light>
 
@@ -8,4 +9,12 @@ export type PlgdThemeType = 'light' | 'siemens'
 export function get(from: object, key: string, def?: any): any {
     const g = lGet(from, key, def)
     return g === '' ? def : g
+}
+
+export const getTheme = (theme: PlgdThemeType) => {
+    if (theme === 'siemens') {
+        return siemens
+    }
+
+    return light
 }
