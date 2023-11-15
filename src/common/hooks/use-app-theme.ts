230a0256a05125eb9095a573b6de23608a63ddf3
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-11-15 22:29:30 +0100

    use-app-theme.ts

---
+++ src/common/hooks/use-app-theme.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AnyAction } from 'redux'
+
+export type UseAppThemeType = {
+    getTheme: (url: string) => any
+    setTheme: (theme: string) => AnyAction
+    setThemes: (themes: string[]) => AnyAction
+}
+
+export function useAppTheme(options: UseAppThemeType) {
+    const { getTheme, setTheme, setThemes } = options
+    const [appTheme, setAppTheme] = useState<null | object[]>(null)
+    const [themeError, setThemeError] = useState<any>(null)
+    const appStore = useSelector((state: any) => state.app)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!appTheme) {
+            const getThemeData = async () => {
+                try {
+                    const { data: themeData } = await getTheme(window.location.origin)
+
+                    if (themeData) {
+                        let themeNames: string[] = []
+                        let themes: any = {}
+
+                        themeData.themes.forEach((t: any) => {
+                            themeNames = themeNames.concat(Object.keys(t))
+                            themes[Object.keys(t)[0]] = t
+                        })
+
+                        if (appStore.configuration?.theme === '') {
+                            dispatch(setTheme(themeData.defaultTheme))
+                        }
+
+                        dispatch(setThemes(themeNames))
+                        setAppTheme(themeData.themes)
+                    }
+                } catch (e) {
+                    console.log(e)
+                    setThemeError(new Error('Could not retrieve the theme file.'))
+                }
+            }
+
+            getThemeData().then()
+        }
+    }, [appStore.configuration?.theme, dispatch, appTheme, getTheme, setThemes, setTheme])
+
+    return [appTheme, themeError]
+}
