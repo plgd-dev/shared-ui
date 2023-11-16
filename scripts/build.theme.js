a0293f2b5b06ab709cd5912160bc906ce43d3a1c
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-11-16 22:54:16 +0100

    theme generator arguments

--- scripts/build.theme.js
+++ scripts/build.theme.js
@@ -1,37 +1,101 @@
 const fs = require('fs-extra')
 const path = require('path')
-const themes = ['plgd']
+const yargs = require('yargs')
+const { promisify } = require('util')
+const childProcess = require('child_process')
 
-if (process.env.NODE_ENV !== 'production') {
-    themes.push('siemens')
-}
+const exec = promisify(childProcess.exec)
 
-const packagePath = process.cwd()
-const buildPath = path.join(packagePath, './build')
+async function run(argv) {
+    const { themes, verbose } = argv
 
-const generalTheme = {
-    defaultTheme: 'plgd',
-    themes: [],
-}
+    const env = {
+        NODE_ENV: 'production',
+        BABEL_ENV: 'production',
+    }
 
-const buildThemePath = path.join(buildPath, 'theme')
-const publicThemePath = path.join(buildPath, '..', '..', '..', 'public', 'theme')
+    const extensions = '--extensions .ts,.js,.tsx,.jsx'
+    const cwd = path.resolve(__dirname, '../')
 
-fs.ensureDirSync(buildThemePath)
-fs.ensureDirSync(publicThemePath)
-
-themes.forEach((theme) => {
-    const buildThemeSourcePath = path.join(buildPath, 'lib', 'theme', theme)
-    const buildColorsSourcePath = path.join(buildPath, 'lib', '_utils', 'colors.js')
-
-    const d = require(buildThemeSourcePath)
-    const colors = require(buildColorsSourcePath)
-
-    fs.writeJsonSync(path.join(buildThemePath, `${theme}.json`), { ...d.default, colorPalette: colors.uiColorMap[theme] })
-
-    generalTheme.themes.push({
-        [theme]: { ...d.default, colorPalette: colors.uiColorMap[theme] },
+    // build theme babel
+    const commandThemeBabel = `babel ./src/components/Atomic/_theme/ -d build/lib/theme ${extensions}`
+    const { stderr, stdout } = await exec(commandThemeBabel, {
+        env: { ...process.env, ...env },
+        cwd,
     })
-})
 
-fs.writeJsonSync(path.join(publicThemePath, `theme.json`), generalTheme)
+    if (stderr) {
+        throw new Error(`'${commandThemeBabel}' failed with \n${stderr}`)
+    }
+
+    if (verbose && stdout) {
+        console.log(stdout)
+    }
+
+    // build theme color
+    const commandThemeColors = `babel ./src/components/Atomic/_utils/colors.ts --out-file build/lib/_utils/colors.js ${extensions}`
+    const { stderrColor, stdoutColor } = await exec(commandThemeColors, {
+        env: { ...process.env, ...env },
+        cwd,
+    })
+
+    if (stderrColor) {
+        throw new Error(`'${commandThemeColors}' failed with \n${stderrColor}`)
+    }
+
+    if (verbose && stdoutColor) {
+        console.log(stdoutColor)
+    }
+
+    const buildPath = path.join(cwd, './build')
+
+    const generalTheme = {
+        defaultTheme: themes[0],
+        themes: [],
+    }
+
+    const buildThemePath = path.join(buildPath, 'theme')
+    const publicThemePath = path.join(buildPath, '..', '..', '..', 'public', 'theme')
+
+    fs.ensureDirSync(buildThemePath)
+    fs.ensureDirSync(publicThemePath)
+
+    themes.forEach((theme) => {
+        const buildThemeSourcePath = path.join(buildPath, 'lib', 'theme', theme)
+        const buildColorsSourcePath = path.join(buildPath, 'lib', '_utils', 'colors.js')
+
+        const d = require(buildThemeSourcePath)
+        const colors = require(buildColorsSourcePath)
+
+        fs.writeJsonSync(path.join(buildThemePath, `${theme}.json`), { ...d.default, colorPalette: colors.uiColorMap[theme] })
+
+        generalTheme.themes.push({
+            [theme]: { ...d.default, colorPalette: colors.uiColorMap[theme] },
+        })
+    })
+
+    fs.writeJsonSync(path.join(publicThemePath, `theme.json`), generalTheme)
+
+    if (verbose) {
+        console.log('Theme generated!')
+    }
+}
+
+yargs
+    .command({
+        command: '$0',
+        description: 'build theme file',
+        builder: (command) =>
+            command
+                .option('themes', {
+                    description: 'Themes array of names.',
+                    type: 'array',
+                    default: ['plgd'],
+                })
+                .option('verbose', { type: 'boolean', default: true }),
+        handler: run,
+    })
+    .help()
+    .strict(true)
+    .version(false)
+    .parse()
--- src/components/Atomic/_theme/index.ts
+++ src/components/Atomic/_theme/index.ts
@@ -3,6 +3,8 @@ import lGet from 'lodash/get'
 
 export type ThemeType = Partial<typeof plgd>
 
+export const defaultTheme = 'plgd'
+
 export function get(from: object, key: string, def?: any): any {
     const g = lGet(from, `colors.${key}`, def)
     return g === '' ? def : g
